cfc852c8b2dc14e5f77fccc057dd05d57532f607
ChenD-VL <duanch@mail.ustc.edu.cn>
2025-03-15 10:31:24 +0800

    vis

--- infer.sh
+++
@@ -1,7 +0,0 @@
-python3 tools/visualize.py  \
---model_path /path/to/model \
---char_dict_pth tools/oclip_char_dict  \
---model_config_file src/training/model_configs/RN50_Seg_Clip.json  \
---img_path ./demo/sample.jpg \
---img_text The text to be reconstructed \  #For example:STIRLING CASTLE
---save_path /path/to/save \
